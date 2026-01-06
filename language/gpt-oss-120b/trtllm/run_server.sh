a1f7b3a69f30dae18cf609fd2cb75de540847753
Shobhit Verma <shobhitv@nvidia.com>
2026-01-06 03:41:39 +0000

    add output_dir flag

--- language/gpt-oss-120b/trtllm/run_server.sh
+++ language/gpt-oss-120b/trtllm/run_server.sh
@@ -5,6 +5,7 @@ for var in $(compgen -v | grep '^SLURM_'); do unset "$var"; done
 model_path=openai/gpt-oss-120b
 extra_args=""
 eagle_path=""
+output_dir=trtllm-serve-logs
 draft_len=3
 
 while [[ $# -gt 0 ]]; do
@@ -17,6 +18,10 @@ while [[ $# -gt 0 ]]; do
             eagle_path=$2
             shift 2
             ;;
+        --output_dir)
+            output_dir=$2
+            shift 2
+            ;;
         --draft_len)
             draft_len=$2
             shift 2
@@ -27,8 +32,9 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+mkdir -p $output_dir
 
-cat <<EOF > config.yml
+cat <<EOF > $output_dir/config.yml
 enable_layerwise_nvtx_marker: false
 stream_interval: 10
 disable_overlap_scheduler: false
@@ -55,7 +61,7 @@ sampler_type: TorchSampler
 EOF
 
 if [ -n "$eagle_path" ]; then
-    cat <<EOF >> config.yml
+    cat <<EOF >> $output_dir/config.yml
 speculative_config:
     decoding_type: Eagle
     max_draft_len: $draft_len
@@ -69,11 +75,7 @@ gpu_count=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 set -x;
 
 for ((gpu=0; gpu<gpu_count; gpu++)); do
-    CUDA_VISIBLE_DEVICES=$gpu trtllm-serve $model_path --host 0.0.0.0 --port 3000$gpu --backend pytorch --max_batch_size 512 --max_num_tokens 4096 --tp_size 1 --ep_size 1 --trust_remote_code --extra_llm_api_options config.yml $extra_args &
+    CUDA_VISIBLE_DEVICES=$gpu trtllm-serve $model_path --host 0.0.0.0 --port 3000$gpu --backend pytorch --max_batch_size 512 --max_num_tokens 4096 --tp_size 1 --ep_size 1 --trust_remote_code --extra_llm_api_options $output_dir/config.yml $extra_args > $output_dir/trtllm-serve-$gpu.log 2>&1 &
 done
-
-# num_servers=2
-# CUDA_VISIBLE_DEVICES=0,1,2,3 TRTLLM_ENABLE_PDL=1 trtllm-serve $model_path --host 0.0.0.0 --port 30000 --backend pytorch --max_batch_size 1024 --tp_size 4 --ep_size 1 --trust_remote_code --extra_llm_api_options config.yml $extra_args & > $output_dir/trtllm-serve-0.log 2>&1
-# CUDA_VISIBLE_DEVICES=4,5,6,7 TRTLLM_ENABLE_PDL=1 trtllm-serve $model_path --host 0.0.0.0 --port 30001 --backend pytorch --max_batch_size 1024 --tp_size 4 --ep_size 1 --trust_remote_code --extra_llm_api_options config.yml $extra_args & > $output_dir/trtllm-serve-1.log 2>&1
 
 wait
